22c4f3addd6f4b820826b03e680ba8c657938fc3
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-11-13 07:36:24 -0700

    Created project

---
+++ DogeDrop/DogeDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogeDropApp.swift
+//  DogeDrop
+//
+//  Created by Smith, Nathanael on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DogeDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
